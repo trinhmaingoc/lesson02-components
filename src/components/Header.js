b25596ed9793c1cc292b0dc2add3f3965fe00837
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-06 22:52:19 +0700

    add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ export class Header extends Component {
         return (
             
             <nav className="navbar navbar-inverse">
-                <a className="navbar-brand" href="/">Bài 2: Component</a>
+                <a className="navbar-brand" href="https://trinhmaingoc.github.io/lesson02-component">Bài 2: Component</a>
                 <ul className="nav navbar-nav">
                     <li className="active">
-                        <a href="/">Trang Chủ</a>
+                        <a href="https://trinhmaingoc.github.io/lesson02-component">Trang Chủ</a>
                     </li>
                     <li>
-                        <a href="/">Danh Mục Sản Phẩm</a>
+                        <a href="https://trinhmaingoc.github.io/lesson01-helloreact">Bài 01 - Hello React</a>
                     </li>
                 </ul>
             </nav>
