a19cae44d42b94cd5ee4efac53094f4769e6287d
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-06 22:49:34 +0700

    Updates

---
+++ precache-manifest.f2e24afcc7c77476b20c66cfbb0e6518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8dd166692de35afda08ac1bf72db87",
+    "url": "/lesson02-component/index.html"
+  },
+  {
+    "revision": "749ddc7b59799b1baccd",
+    "url": "/lesson02-component/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49b7aa022ee21e3aa8a2",
+    "url": "/lesson02-component/static/js/2.a48f507e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lesson02-component/static/js/2.a48f507e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749ddc7b59799b1baccd",
+    "url": "/lesson02-component/static/js/main.5c8b34d8.chunk.js"
+  },
+  {
+    "revision": "e6ce252a29ca74204937",
+    "url": "/lesson02-component/static/js/runtime-main.d4047e60.js"
+  }
+]);
